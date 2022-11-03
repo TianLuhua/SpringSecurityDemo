962a1248142749fdbf1d034eff2b1e0bbd40a1f5
Tianluhua <466469918@qq.com>
2022-11-03 17:24:07 +0800

    init comsumer and product

--- OrderService/src/main/java/com/tlh/security/controller/TestController.java
+++ OrderService/src/main/java/com/tlh/security/controller/TestController.java
@@ -1,4 +1,4 @@
-package com.tlh.security.conterller;
+package com.tlh.security.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
