2edc06fe7a7263ff2ae9a624ad2c6c5c253c4816
Tianluhua <466469918@qq.com>
2022-11-04 17:00:51 +0800

    授权服务器完成

--- AuthClient/src/main/java/com/tlh/security/config/SpringOauthConfig.java
+++ AuthClient/src/main/java/com/tlh/security/config/SpringOauthConfig.java
@@ -13,7 +13,7 @@ import org.springframework.security.oauth2.provider.token.store.redis.RedisToken
 import javax.sql.DataSource;
 
 @Configuration
-public class OauthConfig {
+public class SpringOauthConfig {
 
     @Autowired
     private RedisConnectionFactory redisConnectionFactory;
